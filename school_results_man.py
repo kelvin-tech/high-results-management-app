0f04f475046d3c5c356f8de2afff8b403be578e4
kelvin-tech <41630916+kelvin-tech@users.noreply.github.com>
2021-02-12 07:24:03 +0000

    Add files via upload

---
+++ school_results_man.py
@@ -0,0 +1,39 @@
+# SCHOOL Management app
+
+# grading system
+# 80-100 A
+#70-79 B
+#60-69 C
+#50-59 D
+#40-49 E
+# < 40 F
+
+
+# grade variables
+grade_english = ""
+grade_maths =""
+grade_science =""
+grade_ict =""
+
+# grade values
+grade_a= " A Excellent"
+grade_b=" B Very Good"
+grade_c=" C Good"
+grade_d=" D Credit"
+grade_e=" E Pass"
+grade_f=" F Fail"
+
+# accepting input from users
+
+student_name = input("Enter your name: ")
+student_id = input("Enter your student id: ")
+student_program= input("Enter your program: ")
+student_age = input("Enter your age: ")
+
+#score for courses
+
+score_english = int(input("Enter score for english"))
+score_maths = int(input("Enter score for maths"))
+score_science = int(input("Enter score for science"))
+score_ict = int(input("Enter score for ict"))
+
